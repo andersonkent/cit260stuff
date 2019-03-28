d147a59bc04e4cef9af87db1bdbe25cc7fc89ea2
Courtney Toller <43355629+courtneytoller@users.noreply.github.com>
2019-03-27 19:13:46 -0700

    adding a new print line.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
     public static void main(String[] argv) {
         System.out.println("We are all Java programmers now. Look out, World!");
+        
+        System.out.println("We are now collaborating"); 
     }
 }
